905f2c97b368afbc227de1e9ad3cbc78808b3d95
peteywee <patricktssllc@gmail.com>
2025-08-05 15:28:23 -0500

    vite

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
-// ESM (works because your project already uses "type": "module")
-import tailwind from '@tailwindcss/postcss'
-import autoprefixer from 'autoprefixer'
-
-export default { plugins: [tailwind, autoprefixer] }
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-import App from '@/App'
-import Login from '@/pages/Login'
-import '@/index.css'
-
-const router = createBrowserRouter([
-  { path: '/', element: <App />, children: [
-      { path: 'login', element: <Login /> },
-    ]},
-])
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
